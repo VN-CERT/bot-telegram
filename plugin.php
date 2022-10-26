d2b6f8952a42d51194de988d5fb8c5766d16c386
VNCERT CYBER SECURITY <72367463+VN-CERT@users.noreply.github.com>
2022-10-26 10:34:57 +0700

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -4,9 +4,9 @@ return array(
     'id' =>             'osticket:telegrambot',
     'version' =>        '0.3.1',
     'name' =>           'Telegram Bot',
-    'author' =>         'Tomas Kirkegaard',
+    'author' =>         'Mr.ngoc_it',
     'description' =>    'Notify Telegram on new ticket.',
-    'url' =>            'https://gitlab.com/jfinlay/osticketTelegrambot',
+    'url' =>            'https://github.com/VN-CERT/bot-telegram',
     'plugin' =>         'telegrambot.php:TelegramPlugin',
 );
 
