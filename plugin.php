1bb4f32fc8569ab48864ba11b2c43cadd8379006
VNCERT CYBER SECURITY <72367463+VN-CERT@users.noreply.github.com>
2022-10-26 10:17:21 +0700

    Add files via upload

---
+++ plugin.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+    'id' =>             'osticket:telegrambot',
+    'version' =>        '0.3.1',
+    'name' =>           'Telegram Bot',
+    'author' =>         'Tomas Kirkegaard',
+    'description' =>    'Notify Telegram on new ticket.',
+    'url' =>            'https://gitlab.com/jfinlay/osticketTelegrambot',
+    'plugin' =>         'telegrambot.php:TelegramPlugin',
+);
+
+?>
